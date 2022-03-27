c29418a119f95bb45875970089d76bd57fbe19f3
tuckpuck <tuckertriggs@gmail.com>
2022-03-27 18:21:00 +0200

    add haskell notes

---
+++ emurgo_haskell_course.hs
@@ -0,0 +1,21 @@
+-- This is used to name this in the repl
+module Emurgo_Haskell_Course where
+
+-- Types
+a = 33.33      --Double
+b = 'R'        --Char
+c = "Roberto"  --[Char]
+d = 10         --Integer
+e = (2, "Tucker", 'C', 3.4)--(Integer, [Char], Char, Double)
+f = True       --Bool
+g = [1,2,3,4]  --[Integer]
+
+
+-- For single character use 'c', for double character use "string"
+-- Lists are not linked lists or arrays, though similar to dynamic arrays. Syntax can be deconstructed as 2:3:4:[]
+
+-- :l load
+-- :r refresh
+-- :t type
+-- :? to see all commands
+-- :info to check type classes
